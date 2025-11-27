8a55b73b33979b5c80e3b49b6a090633e9374b86
Carlos Sanchez <krlos.ivan@gmail.com>
2025-11-27 16:04:18 -0600

    comportamiento de titulos
    cambian con scroll

--- sitio-nematodos/estilos/navegacion.js
+++ sitio-nematodos/estilos/navegacion.js
@@ -48,21 +48,59 @@ function verIndice() {
 
 
 /*FUNCIONES INDICE Y NAVEGACION*/
-function irTaxonomia() {
-	window.scroll(
-		{top: (document.getElementById("Taxonomia").offsetTop /*- compensacion*/), left: 0, }
-	);
-	/*verIndice2();*/
-}
 function irIntroduccion() {
 	window.scroll(
-		{top: (document.getElementById("Introduccion").offsetTop /*- compensacion*/), left: 0, }
+		{top: /*0*/(document.getElementById("Introduccion").offsetTop /*- compensacion*/), left: 0, }
+	);
+	/*verIndice2();*/
+	console.log(document.getElementById("Taxonomia").offsetTop);
+}
+
+function irTaxonomia() {
+	window.scroll(
+		{top: /*768*/(document.getElementById("Taxonomia").offsetTop /*- compensacion*/), left: 0, }
+	);
+	/*verIndice2();*/
+	
+}
+
+function irEstructura() {
+	window.scroll(
+		{top: (document.getElementById("Estructura").offsetTop)/*3411*/, left: 0, }
 	);
 	/*verIndice2();*/
 }
-function irEstructura() {
-	window.scroll(
-		{top: (document.getElementById("Estructura").offsetTop /*- compensacion*/), left: 0, }
-	);
-	/*verIndice2();*/
+
+
+window.onscroll = function() {scrollTaxonomia(); scrollEstructura()};
+
+
+function scrollTaxonomia() {
+			var titulo = document.getElementById("Taxonomia");
+			var progreso = titulo.offsetTop - 400;
+	if (window.scrollY > progreso) {
+		document.getElementById("panel1").style.color="transparent";
+		document.getElementById("panel1").style.backgroundColor="#3A3A3A";
+	} else {
+		document.getElementById("panel1").style.color=null;
+		document.getElementById("panel1").style.backgroundColor=null;
+	}
+			
+	/*if (window.scrollY > progreso) {
+				document.getElementById("cabecera").style.opacity=".5";
+			} else {
+				console.log("Y de titulo es "+ titulo.offsetTop +" Y del scroll es "+ window.scrollY);
+			}*/
+}
+
+function scrollEstructura() {
+	var titulo = document.getElementById("Estructura");
+	var progreso = titulo.offsetTop - 400;
+	if (window.scrollY > progreso) {
+		document.getElementById("panel2").style.color="transparent";
+		document.getElementById("panel2").style.backgroundColor="#3A3A3A";
+	} else {
+		document.getElementById("panel2").style.color=null;
+		document.getElementById("panel2").style.backgroundColor=null;
+	}
 }
