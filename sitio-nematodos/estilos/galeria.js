4def5134abe08a27dbef45d52d90568f2843ab74
Carlos Sanchez <krlos.ivan@gmail.com>
2025-12-05 16:05:42 -0600

    hasta PSEUDOCELE
    faltan programar las galerias

--- sitio-nematodos/estilos/galeria.js
+++ sitio-nematodos/estilos/galeria.js
@@ -9,33 +9,67 @@ let slideIndex2 = 1; /*la de prueba*/
 
 function verGaleria() {
 			if (L==1) {
-				document.getElementById("MG1").style.display="block";
-				document.getElementById("MG1").style.opacity="1";
+				document.getElementById("Caja").style.display="block";
+				document.getElementById("Caja").style.opacity="1";
 				document.body.style.overflow = "hidden";
 				L=2;
 				console.log("galeria abierta");
 			} else {
 				cerrarGaleria();
+				
 				/*setTimeout(cerrarGaleria, 100);*/
-				document.getElementById("MG1").style.opacity="0";
+				document.getElementById("Caja").style.opacity="0";
 				document.body.style.overflow = "auto";
 				L=1;
 				console.log("galeria cerrada");
 			}
 }
 
-function cerrarGaleria() {
-	document.getElementById("MG1").style.display=null;
+const LasGalerias = document.getElementsByClassName("galeria");
+function revisar() {let cantidad = LasGalerias.length; console.log("la cantidad de Galerias es "+ cantidad);}
+
+function verCajita(a) {
+	abrirCaja();
+	LasGalerias[a].style.display="block";
 }
 
+function cerrarGaleria() {
+	document.getElementById("Caja").style.display=null;
+}
+
+function abrirCaja() {
+	if (L==1) {
+				document.getElementById("Caja").style.display="block";
+				document.getElementById("Caja").style.opacity="1";
+				document.body.style.overflow = "hidden";
+				L=2;
+				console.log("caja abierta");
+			} else {
+				cerrarCaja();
+				document.getElementById("Caja").style.opacity="0";
+				document.body.style.overflow = "auto";
+				L=1;
+				
+				LasGalerias[0].style.display=null;
+				LasGalerias[1].style.display=null;
+			}
+}
+function cerrarCaja() {
+	console.log("caja cerrada");
+	document.getElementById("Caja").style.display=null;
+}
 //funciones para galeria 
+
+
 function moverSlides(N) {
   mostrarSlides(slideIndex += N);
 }
 function mostrarSlides(N) {
 	let w;
-	const laGaleria = document.getElementById("esquemas1");
+	const laGaleria = document.getElementById("cajita1");
 	let slides = laGaleria.getElementsByClassName("diapositiva");
+	
+	
 	if (N > slides.length) {slideIndex = 1}
 	if (N < 1) {slideIndex = slides.length}
 	for (w = 0; w < slides.length; w++) {
@@ -47,8 +81,8 @@ function mostrarSlides(N) {
 	document.getElementById("contador1").innerHTML = slideIndex;
 }
 
-function cambiarImg(I) {
-	let G = document.getElementById("esquemas1");
+function agrandarImg(I) {
+	let G = document.getElementById("cajita1");
 	let Ima = G.getElementsByTagName("img");
 	let direccion = Ima[I].src;
 	window.open(direccion);
--- sitio-nematodos/estilos/modal.js
+++ sitio-nematodos/estilos/modal.js
@@ -4,6 +4,10 @@
 
 const Fuentes = document.getElementsByClassName("Citado");
 
+function contarFuentes() {
+	let Zaiss = Fuentes[87].innerHTML;
+	alert(Fuentes.length+" ----la fuente num. 87 (ultima) es "+Zaiss);
+}
 function mostrarBiblio() {
 	document.getElementById("Bibliografia").style.display="block";
 	document.body.style.overflow = "hidden";
@@ -15,14 +19,30 @@ function animar() {
 		console.log("animó");
 	}
 
-function citar(b) {
+function citar(A, B, C, D, E, F) {
 	setTimeout(animar, 10);
 	mostrarBiblio();
-	Fuentes[b].style.display="block";
-	document.getElementById("Bibliografia").style.paddingTop="200px";
+	Fuentes[A].style.display="block";
+	/*document.getElementById("Bibliografia").style.paddingTop="200px";*/
+	function restarPadding() {document.getElementById("Bibliografia").style.paddingTop="20px";}
+	
+	if (B === undefined) {B = null; console.log("no hay 2a cita");} else {Fuentes[B].style.display="block";}
+	
+	if (C === undefined)
+	{C = null; console.log("no hay 3a cita");} else {Fuentes[C].style.display="block";}
+	
+	if (D === undefined)
+	{D = null; console.log("no hay 4a cita");} else {Fuentes[D].style.display="block"; restarPadding();}
+	
+	if (E === undefined)
+	{E = null; console.log("ni 5a cita");} else {Fuentes[E].style.display="block"; restarPadding();}
+	
+	if (F === undefined)
+		{F = null; console.log("ni 6a");} else {Fuentes[F].style.display="block"; restarPadding();}
 }
 
-function citarEspecial() {
+
+/*function citarEspecial() {
 	setTimeout(animar, 10);
 	mostrarBiblio();
 	Fuentes[5].style.display="block";
@@ -32,7 +52,7 @@ function citarEspecial() {
 	Fuentes[11].style.display="block";
 	Fuentes[12].style.display="block";
 	document.getElementById("Bibliografia").style.paddingTop="20px";
-}
+}*/
 
 function NoDisplay() {
 	document.getElementById("Bibliografia").style.display="none";
@@ -41,6 +61,7 @@ function salirBiblio() {
 	setTimeout(NoDisplay, 300);
 	document.getElementById("Citas").style.scale=null;
 	document.getElementById("Bibliografia").style.opacity="0";
+	document.getElementById("Bibliografia").style.paddingTop="200px";
 	document.body.style.overflow = "auto";
 	Fuentes[0].style.display="none";
 	Fuentes[1].style.display="none";
@@ -58,4 +79,80 @@ function salirBiblio() {
 	Fuentes[13].style.display="none";
 	Fuentes[14].style.display="none";
 	Fuentes[15].style.display="none";
+	Fuentes[16].style.display="none";
+	Fuentes[17].style.display="none";
+	Fuentes[18].style.display="none";
+	Fuentes[19].style.display="none";
+	Fuentes[20].style.display="none";
+	Fuentes[21].style.display="none";
+	Fuentes[22].style.display="none";
+	Fuentes[23].style.display="none";
+	Fuentes[24].style.display="none";
+	Fuentes[25].style.display="none";
+	Fuentes[26].style.display="none";
+	Fuentes[27].style.display="none";
+	Fuentes[28].style.display="none";
+	Fuentes[29].style.display="none";
+	Fuentes[30].style.display="none";
+	Fuentes[31].style.display="none";
+	Fuentes[32].style.display="none";
+	Fuentes[33].style.display="none";
+	Fuentes[34].style.display="none";
+	Fuentes[35].style.display="none";
+	Fuentes[36].style.display="none";
+	Fuentes[37].style.display="none";
+	Fuentes[38].style.display="none";
+	Fuentes[39].style.display="none";
+	Fuentes[40].style.display="none";
+	Fuentes[41].style.display="none";
+	Fuentes[42].style.display="none";
+	Fuentes[43].style.display="none";
+	Fuentes[44].style.display="none";
+	Fuentes[45].style.display="none";
+	Fuentes[46].style.display="none";
+	Fuentes[47].style.display="none";
+	Fuentes[48].style.display="none";
+	Fuentes[49].style.display="none";
+	Fuentes[50].style.display="none";
+	Fuentes[51].style.display="none";
+	Fuentes[52].style.display="none";
+	Fuentes[53].style.display="none";
+	Fuentes[54].style.display="none";
+	Fuentes[55].style.display="none";
+	Fuentes[56].style.display="none";
+	Fuentes[57].style.display="none";
+	Fuentes[58].style.display="none";
+	Fuentes[59].style.display="none";
+	Fuentes[60].style.display="none";
+	
+	Fuentes[61].style.display="none";
+	Fuentes[62].style.display="none";
+	Fuentes[63].style.display="none";
+	Fuentes[64].style.display="none";
+	Fuentes[65].style.display="none";
+	Fuentes[66].style.display="none";
+	Fuentes[67].style.display="none";
+	Fuentes[68].style.display="none";
+	Fuentes[69].style.display="none";
+	Fuentes[70].style.display="none";
+	
+	Fuentes[71].style.display="none";
+	Fuentes[72].style.display="none";
+	Fuentes[73].style.display="none";
+	Fuentes[74].style.display="none";
+	Fuentes[75].style.display="none";
+	Fuentes[76].style.display="none";
+	Fuentes[77].style.display="none";
+	Fuentes[78].style.display="none";
+	Fuentes[79].style.display="none";
+	Fuentes[80].style.display="none";
+	
+	Fuentes[81].style.display="none";
+	Fuentes[82].style.display="none";
+	Fuentes[83].style.display="none";
+	Fuentes[84].style.display="none";
+	Fuentes[85].style.display="none";
+	Fuentes[86].style.display="none";
+	Fuentes[87].style.display="none";
+
 }
