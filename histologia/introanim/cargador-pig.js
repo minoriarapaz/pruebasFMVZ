86f01f7b8f1b4c63ea74f1048fea2716f973f6ff
Carlos Sanchez <krlos.ivan@gmail.com>
2026-02-26 11:50:18 -0600

    borrado del anterior histologia

---
+++ histologia/introanim/cargador-pig.js
@@ -0,0 +1,201 @@
+(function (cjs, an) {
+
+var p; // shortcut to reference prototypes
+var lib={};var ss={};var img={};
+lib.ssMetadata = [];
+
+
+(lib.AnMovieClip = function(){
+	this.actionFrames = [];
+	this.ignorePause = false;
+	this.gotoAndPlay = function(positionOrLabel){
+		cjs.MovieClip.prototype.gotoAndPlay.call(this,positionOrLabel);
+	}
+	this.play = function(){
+		cjs.MovieClip.prototype.play.call(this);
+	}
+	this.gotoAndStop = function(positionOrLabel){
+		cjs.MovieClip.prototype.gotoAndStop.call(this,positionOrLabel);
+	}
+	this.stop = function(){
+		cjs.MovieClip.prototype.stop.call(this);
+	}
+}).prototype = p = new cjs.MovieClip();
+// symbols:
+
+
+
+// stage content:
+(lib.cargadorpig = function(mode,startPosition,loop,reversed) {
+if (loop == null) { loop = true; }
+if (reversed == null) { reversed = false; }
+	var props = new Object();
+	props.mode = mode;
+	props.startPosition = startPosition;
+	props.labels = {};
+	props.loop = loop;
+	props.reversed = reversed;
+	cjs.MovieClip.apply(this,[props]);
+
+	// dot
+	this.shape = new cjs.Shape();
+	this.shape.graphics.f("#20B2AA").s().p("AghAjQgOgPAAgUQAAgTAOgOQAOgOATAAQAUAAAOAOQAOAOAAATQAAAUgOAPQgOANgUAAQgTAAgOgNg");
+	this.shape.setTransform(45.525,41.9);
+
+	this.shape_1 = new cjs.Shape();
+	this.shape_1.graphics.f("#20B2AA").s().p("AAyAjQgNgPAAgUQAAgTANgOQAPgOAUAAQATAAAPAOQAOAOAAATQAAAUgOAPQgPANgTAAQgUAAgPgNgAh2AjQgOgPAAgUQAAgTAOgOQAOgOAUAAQAUAAAPAOQANAOAAATQAAAUgNAPQgPANgUAAQgUAAgOgNg");
+	this.shape_1.setTransform(54,41.9);
+
+	this.shape_2 = new cjs.Shape();
+	this.shape_2.graphics.f("#20B2AA").s().p("ACHAjQgOgPAAgUQAAgTAOgOQAPgOATAAQAUAAAPAOQAOAOAAATQAAAUgOAPQgPANgUAAQgTAAgPgNgAghAjQgOgPAAgUQAAgTAOgOQAOgOATAAQAUAAAOAOQAOAOAAATQAAAUgOAPQgOANgUAAQgTAAgOgNgAjLAjQgOgPAAgUQAAgTAOgOQAPgOATAAQAUAAAPAOQAOAOAAATQAAAUgOAPQgPANgUAAQgTAAgPgNg");
+	this.shape_2.setTransform(62.475,41.9);
+
+	this.shape_3 = new cjs.Shape();
+	this.shape_3.graphics.f("#20B2AA").s().p("ADcAjQgOgPAAgUQAAgTAOgOQAOgOAUAAQAUAAAPAOQANAOAAATQAAAUgNAPQgPANgUAAQgUAAgOgNgAAyAjQgOgPABgUQgBgTAOgOQAPgOATAAQAUAAAPAOQAOAOAAATQAAAUgOAPQgPANgUAAQgTAAgPgNgAh2AjQgOgPAAgUQAAgTAOgOQAOgOAUAAQAUAAAOAOQAOAOABATQgBAUgOAPQgOANgUAAQgUAAgOgNgAkfAjQgOgPgBgUQABgTAOgOQAOgOAUAAQATAAAPAOQAOAOAAATQAAAUgOAPQgPANgTAAQgUAAgOgNg");
+	this.shape_3.setTransform(70.95,41.9);
+
+	this.shape_4 = new cjs.Shape();
+	this.shape_4.graphics.f("#20B2AA").s().p("AAzAjQgOgPgBgUQABgTAOgOQAOgOAUAAQATAAAPAOQAOAOAAATQAAAUgOAPQgPANgTAAQgUAAgOgNgAh2AjQgOgPAAgUQAAgTAOgOQAPgOATAAQAUAAAPAOQAOAOgBATQABAUgOAPQgPANgUAAQgTAAgPgNg");
+	this.shape_4.setTransform(87.9,41.9);
+
+	this.timeline.addTween(cjs.Tween.get({}).to({state:[{t:this.shape,p:{x:45.525}}]}).to({state:[{t:this.shape_1}]},3).to({state:[{t:this.shape_2,p:{x:62.475}}]},3).to({state:[{t:this.shape_3}]},3).to({state:[{t:this.shape_2,p:{x:79.425}}]},3).to({state:[{t:this.shape_4}]},3).to({state:[{t:this.shape,p:{x:96.375}}]},3).to({state:[]},3).wait(3));
+
+	// cerdoblanco
+	this.shape_5 = new cjs.Shape();
+	this.shape_5.graphics.lf(["#FFFFFF","#483D8B"],[0,1],-5.1,-18.5,-5.1,161.4).s().p("ADsHmQgIgCgJgDQgTgHgFgLQgIgPgIggQgIgkAEgNQAAgLgEgHIgGgMQgHgSAJg2IABgKQgeA0gaAaQgLALgCAqQgBAfADAXQABALANANQANAOABAIIhRAAIgPgQQgPgRgCgHQgDgLAGgbQAHgfAOgSQADgYgBgXIgCgjIgDgSQiuBZi6gjQgigHghgKQACAPAHAYQAMAjALARQAFAGAQANIAfAZQAqAhAAANIhIAAQgHABgMgFQgYgKgVgcQgWgcgYgxIgUgqIgPgUQgRgUgHgGQgFgDgEgOQgDgJgBgLIgFgEIgUAgQgNAUAAAJQgCAUALAmQAKAgAPAdQAKAWAZAbIAWAWIhyAAIgFgMQgGgPgDgSQgEgSgRgmIgXgwQgKgTgGgeQgHgnAKgVQANgcgQghQgJgRgagiIgFgHIgCgDQhAhWgIheQgEgyANg6IAFgQQgMgFgKAeQgGARgIAiQgEANgJATQgFARAEAZQAEAZgTAXQgJAKgKAHIgPgDQgQAOgHAIQgLAOgIAUQgLgbAQgnQAJgXARgaQAFgHAHgTQAGgRAIgIQAPgPALgSQAQgaAAgUQAAgXAIgRQAHgQAMgEQAJgEAbgdQAOgOALgOQAGgKAdgSQAegSAmgRQBfgtA8AAQAvAABzATIBTAMQAqAGAXAAQAwAABxgXQBtgXAegNQAggQAtgMQA4gQA+gIQA8gHAxAfQATALANAPQAMANADALQAZgCAdAUIAXAQQANALAHADQAOACA5AYIA2AXIB9ACIABgGQACgGAJAAQAQAJAIAoQAEAUABATIgGAAQgGABgEAGQglBJhoAxQglARgxAQIhIAVQhEBPhwA+QgwAagpAQQADAKgEAEQgDAEgPAvQgPArgCALQgCAIgSAoIgVAvQgCAEAAANQAAANADAPQAIAoAUATQAHAHAQAMQALAJAAAJg");
+	this.shape_5.setTransform(74.6017,54.0182,0.6778,0.6778);
+
+	this.timeline.addTween(cjs.Tween.get(this.shape_5).wait(24));
+
+	this._renderFirstFrame();
+
+}).prototype = p = new lib.AnMovieClip();
+p.nominalBounds = new cjs.Rectangle(88.6,71.1,47,15.900000000000006);
+// library properties:
+lib.properties = {
+	id: 'F2ED7B4A5CED41B18974CB59BD846E6D',
+	width: 150,
+	height: 100,
+	fps: 15,
+	color: "#20B2AA",
+	opacity: 1.00,
+	manifest: [],
+	preloads: []
+};
+
+
+
+// bootstrap callback support:
+
+(lib.Stage = function(canvas) {
+	createjs.Stage.call(this, canvas);
+}).prototype = p = new createjs.Stage();
+
+p.setAutoPlay = function(autoPlay) {
+	this.tickEnabled = autoPlay;
+}
+p.play = function() { this.tickEnabled = true; this.getChildAt(0).gotoAndPlay(this.getTimelinePosition()) }
+p.stop = function(ms) { if(ms) this.seek(ms); this.tickEnabled = false; }
+p.seek = function(ms) { this.tickEnabled = true; this.getChildAt(0).gotoAndStop(lib.properties.fps * ms / 1000); }
+p.getDuration = function() { return this.getChildAt(0).totalFrames / lib.properties.fps * 1000; }
+
+p.getTimelinePosition = function() { return this.getChildAt(0).currentFrame / lib.properties.fps * 1000; }
+
+an.bootcompsLoaded = an.bootcompsLoaded || [];
+if(!an.bootstrapListeners) {
+	an.bootstrapListeners=[];
+}
+
+an.bootstrapCallback=function(fnCallback) {
+	an.bootstrapListeners.push(fnCallback);
+	if(an.bootcompsLoaded.length > 0) {
+		for(var i=0; i<an.bootcompsLoaded.length; ++i) {
+			fnCallback(an.bootcompsLoaded[i]);
+		}
+	}
+};
+
+an.compositions = an.compositions || {};
+an.compositions['F2ED7B4A5CED41B18974CB59BD846E6D'] = {
+	getStage: function() { return exportRoot.stage; },
+	getLibrary: function() { return lib; },
+	getSpriteSheet: function() { return ss; },
+	getImages: function() { return img; }
+};
+
+an.compositionLoaded = function(id) {
+	an.bootcompsLoaded.push(id);
+	for(var j=0; j<an.bootstrapListeners.length; j++) {
+		an.bootstrapListeners[j](id);
+	}
+}
+
+an.getComposition = function(id) {
+	return an.compositions[id];
+}
+
+
+an.makeResponsive = function(isResp, respDim, isScale, scaleType, domContainers) {		
+	var lastW, lastH, lastS=1;		
+	window.addEventListener('resize', resizeCanvas);		
+	resizeCanvas();		
+	function resizeCanvas() {			
+		var w = lib.properties.width, h = lib.properties.height;			
+		var iw = window.innerWidth, ih=window.innerHeight;			
+		var pRatio = window.devicePixelRatio || 1, xRatio=iw/w, yRatio=ih/h, sRatio=1;			
+		if(isResp) {                
+			if((respDim=='width'&&lastW==iw) || (respDim=='height'&&lastH==ih)) {                    
+				sRatio = lastS;                
+			}				
+			else if(!isScale) {					
+				if(iw<w || ih<h)						
+					sRatio = Math.min(xRatio, yRatio);				
+			}				
+			else if(scaleType==1) {					
+				sRatio = Math.min(xRatio, yRatio);				
+			}				
+			else if(scaleType==2) {					
+				sRatio = Math.max(xRatio, yRatio);				
+			}			
+		}
+		domContainers[0].width = w * pRatio * sRatio;			
+		domContainers[0].height = h * pRatio * sRatio;
+		domContainers.forEach(function(container) {				
+			container.style.width = w * sRatio + 'px';				
+			container.style.height = h * sRatio + 'px';			
+		});
+		stage.scaleX = pRatio*sRatio;			
+		stage.scaleY = pRatio*sRatio;
+		lastW = iw; lastH = ih; lastS = sRatio;            
+		stage.tickOnUpdate = false;            
+		stage.update();            
+		stage.tickOnUpdate = true;		
+	}
+}
+an.handleSoundStreamOnTick = function(event) {
+	if(!event.paused){
+		var stageChild = stage.getChildAt(0);
+		if(!stageChild.paused || stageChild.ignorePause){
+			stageChild.syncStreamSounds();
+		}
+	}
+}
+an.handleFilterCache = function(event) {
+	if(!event.paused){
+		var target = event.target;
+		if(target){
+			if(target.filterCacheList){
+				for(var index = 0; index < target.filterCacheList.length ; index++){
+					var cacheInst = target.filterCacheList[index];
+					if((cacheInst.startFrame <= target.currentFrame) && (target.currentFrame <= cacheInst.endFrame)){
+						cacheInst.instance.cache(cacheInst.x, cacheInst.y, cacheInst.w, cacheInst.h);
+					}
+				}
+			}
+		}
+	}
+}
+
+
+})(createjs = createjs||{}, AdobeAn = AdobeAn||{});
+var createjs, AdobeAn;
